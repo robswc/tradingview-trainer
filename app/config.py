95a05947f2daff1c43c7b0e66dd55ef5ef96dc5a
Robswc <robert.s.w.carroll@gmail.com>
2019-03-22 23:28:04 -0400

    first commit

---
+++ app/config.py
@@ -0,0 +1,11 @@
+# set 'un' to 'MANUAL' like so:
+# un = 'MANUAL' if you wish to log in manually, rather than have credentials
+# stored in the config file.
+
+un = 'REPLACE W/YOUR USERNAME'
+pw = 'REPLACE W/YOUR PASSWORD'
+fee = 0.16
+initial_amount = 10000
+
+# Use larger number for slower internet.
+sleep = 2
