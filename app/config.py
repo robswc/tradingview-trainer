43f566c0a4c855272e037fd4c05f503a40eca329
Robswc <robert.s.w.carroll@gmail.com>
2019-07-08 20:31:48 -0400

    updated config.py

--- app/config.py
+++ app/config.py
@@ -4,8 +4,34 @@
 
 un = 'MANUAL'
 pw = 'MANUAL'
-fee = 0.16
+fee = 0.075
 initial_amount = 10000
+
+# Custom xpaths
+
+custom_xpath_replay = '/html/body/div[9]/div/div[2]/div[3]/div'
+custom_xpath_play_replay = 'a'
+custom_xpath_last_price = '/html/body/div[1]/div[1]/div[3]/div[1]/div/table/tr[1]/td[2]/div/div[3]/div[1]/div/span[4]/span[2]'
+
+# GUI
+
+font = "Arial"
+font_weight = "bold"
+font_size = 14
+
+
+background_color = "#252526"
+button_color = "#2D2D2D"
+button_color_light = "#3C3C3C"
+textarea_color = "#1E1E1E"
+
+color_white = "#D4D4D4"
+color_grey = "#D4D4D4"
+color_dark_grey = "#818181"
+color_green = "#26A69A"
+color_red = "#EF5350"
 
 # Use larger number for slower internet.
 sleep = 2
+
+
